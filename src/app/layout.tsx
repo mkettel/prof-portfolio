c8e5bd2fa440c11eb55a814b7269b58fe021caeb
mkettel <mattkettelkamp@gmail.com>
2026-06-05 01:35:14 -0700

    updated to add coati

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,36 +5,60 @@ import { Inter } from "next/font/google";
 import { twMerge } from "tailwind-merge";
 import { Footer } from "@/components/Footer";
 import { ThemeProvider } from "next-themes";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
-import { Metadata } from 'next'
+import { Metadata } from "next";
 
 const metadata: Metadata = {
   title: {
-    default: 'Matt Kettelkamp | Developer, Skier, and Person',
-    template: '%s | Matt Kettelkamp Portfolio Website'
+    default: "Matt Kettelkamp | Developer, Skier, and Person",
+    template: "%s | Matt Kettelkamp Portfolio Website",
   },
-  description: 'Matt Kettelkamp is a developer, skier, and a passionate person who likes making digital experiences. ',
-  keywords: ['Nextjs', 'Javascript', 'React', 'TailwindCSS', 'Vercel', 'Developer', 'Portfolio', 'web developer', 'web development', 'software engineer', 'software development', 'software developer', 'skier', 'skiing', 'outdoors', 'adventure', 'adventurer', 'adventurous', 'adventure seeker', 'adventure enthusiast'],
-  metadataBase: new URL('https://mattkettelkamp.com'),
+  description:
+    "Matt Kettelkamp is a developer, skier, and a passionate person who likes making digital experiences. ",
+  keywords: [
+    "Nextjs",
+    "Javascript",
+    "React",
+    "TailwindCSS",
+    "Vercel",
+    "Developer",
+    "Portfolio",
+    "web developer",
+    "web development",
+    "software engineer",
+    "software development",
+    "software developer",
+    "skier",
+    "skiing",
+    "outdoors",
+    "adventure",
+    "adventurer",
+    "adventurous",
+    "adventure seeker",
+    "adventure enthusiast",
+  ],
+  metadataBase: new URL("https://mattkettelkamp.com"),
   openGraph: {
-    title: 'Matt Kettelkamp | Developer, Skier, and Person',
-    description: 'Matt Kettelkamp is a developer, skier, and a passionate person who likes making digital experiences. ',
-    url: 'https://mattkettelkamp.com',
-    siteName: 'Matt Kettelkamp',
-    locale: 'en_US',
-    type: 'website',
+    title: "Matt Kettelkamp | Developer, Skier, and Person",
+    description:
+      "Matt Kettelkamp is a developer, skier, and a passionate person who likes making digital experiences. ",
+    url: "https://mattkettelkamp.com",
+    siteName: "Matt Kettelkamp",
+    locale: "en_US",
+    type: "website",
   },
   robots: {
     index: true,
     follow: true,
   },
   twitter: {
-    card: 'summary_large_image',
-    title: 'Matt Kettelkamp | Developer, Skier, and Person',
-    description: 'Matt Kettelkamp is a developer, skier, and a passionate person who likes making digital experiences. ',
-  }
-}
+    card: "summary_large_image",
+    title: "Matt Kettelkamp | Developer, Skier, and Person",
+    description:
+      "Matt Kettelkamp is a developer, skier, and a passionate person who likes making digital experiences. ",
+  },
+};
 
 const inter = Inter({
   subsets: ["latin"],
@@ -51,7 +75,7 @@ export default function RootLayout({
       <body
         className={twMerge(
           inter.className,
-          "flex antialiased h-screen overflow-hidden bg-gray-100 dark:bg-zinc-900 duration-300 transition-colors"
+          "flex antialiased h-screen overflow-hidden bg-gray-100 dark:bg-zinc-900 duration-300 transition-colors",
         )}
       >
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
@@ -64,6 +88,11 @@ export default function RootLayout({
           </div>
           <Analytics />
           <GoogleAnalytics />
+          <script
+            defer
+            data-site="site_0pnmu78fmvex"
+            src="https://coati.vercel.app/p.js"
+          ></script>
         </ThemeProvider>
       </body>
     </html>
